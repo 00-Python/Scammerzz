4280cc5d3de667a3d4eb9d2879c0ad7f64f7e7e9
Joe <rwc.webster@gmail.com>
2023-06-11 03:42:47 +0100

    converted code to use functions

--- cli.py
+++ cli.py
@@ -1,27 +1,48 @@
 from api import ScamSearchAPI
+import json
+import os
 
-apikey = ""
-search_term = "1PQdgvRNF4jhX1PfkkcBd8tr9NMpsCvwz8"
-search_type
-res = ScamSearchAPI(apikey).search_exact_match(search_term, search_type)
-length = len(res['result'])
-res = json.dumps(res)
-res = json.loads(res)
+# function to perform exact search on ScamSearchAPI
+def exact_search_scams(apikey, search_term, search_type):
+    res = ScamSearchAPI(apikey).search_exact_match(search_term, search_type)
+    res = json.dumps(res)
+    res = json.loads(res)
+
+    if res['status'] == 'False':
+        return False
+
+    return res
+
+# function to print the results of exact search
+def print_exact_search_scams(res):
+    length = len(res['result'])
+    for item in res['result']:
+        print("Bitcoin Address:", item['bitcoin_address'])
+        print("Scammer Type:", item['scammertype'])
+        print("Scammer Type Other:", item['scammertype_other'])
+        print("Scammer Username:", item['scammerusername'])
+        print("Scammer Email:", item['scammeremail'])
+        print("Scammer Phone:", item['scammerphone'])
+        print("Scam Description:", item['scamdescription'])
+        print("Created At:", item['created_at'])
+        print("----------------------------------------------------------------------")
+
+    print(f'{length} results')
+    
 
 
-if res['status'] == 'False':
-    print("No results, please refine your search paramater")
+if __name__ == '__main__':
+    # get the API key from environment variable or file
+    api_key = os.environ.get('SS_API_KEY')
+    if not api_key:
+        with open('api.txt', 'r') as f:
+            api_key = f.read().strip()
 
+    # get the search term and search type from user input
+    search_term = input("Enter the search term: ")
+    search_type = input("Enter the search type: ")
 
-for item in res['result']:
-    print("Bitcoin Address:", item['bitcoin_address'])
-    print("Scammer Type:", item['scammertype'])
-    print("Scammer Type Other:", item['scammertype_other'])
-    print("Scammer Username:", item['scammerusername'])
-    print("Scammer Email:", item['scammeremail'])
-    print("Scammer Phone:", item['scammerphone'])
-    print("Scam Description:", item['scamdescription'])
-    print("Created At:", item['created_at'])
-    print("----------------------------------------------------------------------")
-
-print(f'{length} results')
+    # perform exact search and print the results
+    res = exact_search_scams(api_key, search_term, search_type)
+    if False == res: print("No Results, please refine your search parameter")
+    print_exact_search_scams(res)
